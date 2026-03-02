ead01f2c84ca969c515dd8896c99e6e6e16a40cf
Akash Yadav <akash.y@neosoftmail.com>
2026-03-02 11:20:58 +0530

    Initial commit

---
+++ files/forms/contact.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Requires the "PHP Email Form" library
+ * The "PHP Email Form" library is available only in the pro version of the template
+ * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+ * For more info and help: https://bootstrapmade.com/php-email-form/
+ */
+
+// Replace contact@example.com with your real receiving email address
+// $receiving_email_address = 'adityasoniharshad@gmail.com';
+
+// if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+//   include($php_email_form);
+// } else {
+//   die('Unable to load the "PHP Email Form" Library!');
+// }
+
+// $contact = new PHP_Email_Form;
+// $contact->ajax = true;
+
+// $contact->to = $receiving_email_address;
+// $contact->from_name = $_POST['name'];
+// $contact->from_email = $_POST['email'];
+// $contact->subject = $_POST['subject'];
+
+// Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+/*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+// $contact->add_message($_POST['name'], 'From');
+// $contact->add_message($_POST['email'], 'Email');
+// $contact->add_message($_POST['message'], 'Message', 10);
+
+// echo $contact->send();
+
+$to = "adityasoniharshad@gmail.com";
+$subject = "Test Email";
+$message = "Hello, this is a test email.";
+
+$headers = "From: " . $_POST['email'] . "\r\n";
+$headers .= "Reply-To: " . $_POST['email'] . "\r\n";
+$headers .= "CC: janusoni1997@gmail.com\r\n";
+//$headers .= "BCC: bccperson@example.com\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+if (mail($to, $subject, $message, $headers)) {
+  echo "Email sent successfully!";
+} else {
+  echo "Email sending failed.";
+}
